fe19836620534cc83210ff81b3406a17200ea29f
Cecilia Torreira <celia.umbreon@gmail.com>
2023-05-28 15:57:51 +0100

    No conseguí implementar bien el boton de borrar comida, estuve revisándolo varias veces pero no conseguí sacarlo

--- src/App.js
+++ src/App.js
@@ -3,26 +3,51 @@ import './App.css';
 import foods from './foods.json';
 import FoodBox from './components/FoodBox';
 import AddFoodForm from './components/AddFoodForm';
+import Search from './components/Search';
 
 function App() {
   const [foodList, setFoodList] = useState(foods);
 
+  // //! Estado para la búsqueda de comida
+  const [filteredFoundFood, setFoundFood] = useState(foods);
+  // Arreglado problema por el cual al principio no me mostraba nada hasta que buscase alguna comida.
+  
   const addFood = function (newFood) {
     const newFoodList = [...foodList, newFood];
     setFoodList(newFoodList);
   };
 
+  const searchFood = (search) => {
+    const foundFood = foods.filter((eachFood) => {
+      return eachFood.name.toLowerCase().includes(search.toLowerCase());
+    });
+
+    setFoundFood(foundFood);
+  };
+
+
+  const deleteFood =(index) =>{
+    console.log("Borrando comida"); // Funciona
+  // Nuevo array
+    const newFoodList = [...foodList]
+    newFoodList.splice(index, 1)
+    setFoodList(newFoodList)
+  
+  };
+
+
   return (
     <div className="App">
       <AddFoodForm addFood={addFood} />
-
       {/* Me olvidé de añadirle la función como props para que el componente pueda llamarla */}
+
+      <Search searchFood={searchFood} />
 
       {/* Tenemos un array y hacemos un .map() */}
       <h3>Food List</h3>
       <section className="food-list">
-        {foodList.map((eachFood, index) => (
-          <FoodBox key={index} food={eachFood} />
+        {filteredFoundFood.map((eachFood, index) => (
+          <FoodBox key={index} food={eachFood} deleteFood={deleteFood} />
         ))}
       </section>
     </div>
