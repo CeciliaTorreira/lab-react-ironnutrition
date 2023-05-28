01c78b7b40278870d8dba314c2a446d8a2095f94
Cecilia Torreira <celia.umbreon@gmail.com>
2023-05-28 14:32:16 +0100

    Iteración 4 completada

--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@ import AddFoodForm from './components/AddFoodForm';
 
 function App() {
   const [foodList, setFoodList] = useState(foods);
-  
 
+  const addFood = function (newFood) {
+    const newFoodList = [...foodList, newFood];
+    setFoodList(newFoodList);
+  };
 
   return (
     <div className="App">
+      <AddFoodForm addFood={addFood} />
 
-   <AddFoodForm/>
+      {/* Me olvidé de añadirle la función como props para que el componente pueda llamarla */}
 
-
-
-
-  {/* Tenemos un array y hacemos un .map() */}
-    <h3 >Food List</h3>
+      {/* Tenemos un array y hacemos un .map() */}
+      <h3>Food List</h3>
       <section className="food-list">
-                {foodList.map((eachFood, index) => (
+        {foodList.map((eachFood, index) => (
           <FoodBox key={index} food={eachFood} />
         ))}
       </section>
--- src/components/AddFoodForm.jsx
+++ src/components/AddFoodForm.jsx
@@ -1,54 +1,87 @@
 import { useState } from 'react';
-
-function AddFoodForm() {
+//No conseguía que me funcionase al pasarle la función como prop pero al destructurarla me funcionó
+function AddFoodForm({ addFood }) {
   const [name, setName] = useState('');
   const [image, setImage] = useState('');
   const [calories, setCalories] = useState('');
   const [servings, setServings] = useState('');
   // Podemos crear un estado diferente para cada componente o hacer uno solo
-//   const [formValues, setFormValues] = useState({
-//   name: "", 
-// calories: 0, 
-// image: "",
-// servings: 0})
- 
+  //   const [formValues, setFormValues] = useState({
+  //   name: "",
+  // calories: 0,
+  // image: "",
+  // servings: 0})
 
-// const eventhandlers aquí
-// Pensar en implementar capitalized + clausula
+  // const eventhandlers aquí
 
-// funcion de handleSubmit 
+  const handleNameInput = function (e) {
+    setName(e.target.value);
+  };
+
+  const handleImageInput = function (e) {
+    setImage(e.target.value);
+  };
+
+  const handleCaloriesInput = function (e) {
+    setCalories(e.target.value);
+  };
+
+  const handleServingsInput = function (e) {
+    setServings(e.target.value);
+  };
+
+  // Pensar en implementar capitalized + clausula
+
+  // funcion de handleSubmit
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+
+    const newFood = { name, image, calories, servings };
+    console.log(newFood);
+    addFood(newFood);
+
+    setName('');
+    setImage('');
+    setCalories('');
+    setServings('');
+  };
+  console.log('Adding food'); /// Funciona
 
   return (
     <div>
       <h3>Add new food</h3>
-      <form>
-      <label value={name} htmlFor="name">Food name:</label>
-        <input type="text" name="name"/>
+      <form onSubmit={handleSubmit}>
+        <label value={name} htmlFor="name">
+          Food name:
+        </label>
+        <input type="text" name="name" onChange={handleNameInput} />
         <br />
 
-        <label value={calories} htmlFor="calories">Calories:</label>
-        <input type="number" name="calories"/>
+        <label value={calories} htmlFor="calories">
+          Calories:
+        </label>
+        <input type="number" name="calories" onChange={handleCaloriesInput} />
         <br />
-        <label value={servings} htmlFor="servings">Servings:</label>
-        <input type="number" name="servings"/>
+        <label value={servings} htmlFor="servings">
+          Servings:
+        </label>
+        <input type="number" name="servings" onChange={handleServingsInput} />
         <br />
 
-        <label value={image} htmlFor="image">Picture:</label>
-        <input type="text" name="image"/>
-<br />
-     <button>Add to the list</button>
+        <label value={image} htmlFor="image">
+          Picture:
+        </label>
+        <input type="text" name="image" onChange={handleImageInput} />
+        <br />
+        <button type="submit">Add to the list</button>
       </form>
-      
     </div>
   );
 }
-
 
 export default AddFoodForm;
 
 //onChange event
 
-
 // setValues ...formValues, event.target.value (e)
-
-
